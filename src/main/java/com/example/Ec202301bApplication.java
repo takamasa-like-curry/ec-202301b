a371463cb586260378b371bc4b0da8f146910926
takamasa-like-curry <sugaharatakamasa@sugawaratakamasanoMacBook-Pro.local>
2023-02-27 17:06:37 +0900

    新規プロジェクトを作成しました。

---
+++ src/main/java/com/example/Ec202301bApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202301bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202301bApplication.class, args);
+	}
+
+}
