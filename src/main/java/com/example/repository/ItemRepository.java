c5816c6a4ca3332225da3a64620ea52be86bd949
koki.0318 <wk.bass0318@gmail.com>
2023-02-28 15:27:22 +0900

    合間検索を行うメソッドを追加

--- src/main/java/com/example/repository/ItemRepository.java
+++ src/main/java/com/example/repository/ItemRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Item;
@@ -26,9 +28,21 @@ public class ItemRepository {
 	 * 商品に一覧を表示します
 	 * @return　一覧情報
 	 */
-	public List<Item> findAll(){
+	List<Item> findAll(){
 		String sql="SELECT id,name,description,price_m,price_l,image_path,deleted From items;";
 		List<Item> itemList=template.query(sql, ITEM_ROW_MAPPER);
 		return itemList;
 	}
+	/**
+	 * 商品の名前を曖昧検索します
+	 * @param name
+	 * @return　曖昧検索後の一覧情報
+	 */
+	public List<Item> searchByNameFuzzy(String name){
+		String sql="SELECT id,name,description,price_m,price_l,image_path,deleted FROM items WHERE name LIKE :name ORDER BY price_m ASC;";
+		SqlParameterSource param=new MapSqlParameterSource().addValue("name","%"+name+"%");
+		List<Item> itemList=template.query(sql,param,ITEM_ROW_MAPPER);
+		return itemList;
+	}
+	
 }
