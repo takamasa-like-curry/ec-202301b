d0a9043d0e6c4567a5b3aee47aeb00dc875793db
hayashiasu <hxasuka.0724@gmail.com>
2023-03-01 15:39:07 +0900

    ordersテーブルを操作するリポジトリを作成しました

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,5 +1,47 @@
 package com.example.repository;
 
-public class Order {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
+import com.example.domain.Order;
+
+/**
+ * ordersテーブルを操作するリポジトリ.
+ * 
+ * @author hayashiasuka
+ *
+ */
+@Repository
+public class OrderRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private static final String TABLE_NAME = "orders";
+	private static final RowMapper<Order> ORDER_ROW_MAPPER = new BeanPropertyRowMapper<>(Order.class);
+
+	/**
+	 * 主キーで注文情報を取得する.
+	 * 
+	 * @param id 注文ID
+	 * @return 検索された注文情報
+	 */
+	public Order load(Integer id) {
+
+		String sql = "SELECT id, user_id, status, total_price, order_date, destination_name, destination_email, "
+				+ "destination_zipcode, destination_address, destination_tel, delivery_time, payment_method FROM "
+				+ TABLE_NAME + " WHERE id=:id;";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+
+		Order order = template.queryForObject(sql, param, ORDER_ROW_MAPPER);
+
+		return order;
+
+	}
 }
