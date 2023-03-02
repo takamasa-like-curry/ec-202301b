ebacd30f11a4e9c62ad4df2b02953c72ba1d7596
hayashiasu <hxasuka.0724@gmail.com>
2023-03-02 09:19:04 +0900

    注文確認画面表示の処理を制御するコントローラを作成しました

---
+++ src/main/java/com/example/controller/OrderConfirmController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Order;
+import com.example.service.OrderConfirmService;
+
+/**
+ * 注文確認画面表示の機能を制御するコントローラ.
+ * 
+ * @author hayashiasuka
+ *
+ */
+@Controller
+@RequestMapping("/orderConfirm")
+public class OrderConfirmController {
+
+	
+	@Autowired
+	private OrderConfirmService orderConfirmService;
+	
+	/**
+	 * 注文確認画面を表示する.
+	 * 
+	 * @param orderId 注文ID
+	 * @return 注文確認画面
+	 */
+	public String showOrderConfirm(Integer orderId, Model model) {
+		
+		Order order = orderConfirmService.showOrderConfirm(orderId);
+		model.addAttribute("order", order);
+		
+		return "order_confirm";
+		
+	}
+}
