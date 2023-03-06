cb03ca11e81773bc34029a09f200f005317d18c9
koki.0318 <wk.bass0318@gmail.com>
2023-03-06 09:26:26 +0900

    不要なjsファイルを削除しました

--- src/main/resources/static/js/all_select.js
+++
@@ -1,9 +0,0 @@
-/*$(function () {
-  $(".select_btn").on("click", function () {
-    $(".checkbox").prop("checked", true);
-
-  });
-  $(".delete_btn").on("click", function () {
-    $(".checkbox").prop("checked", false);
-  });
-});*/
